9a177d085a13971cc30c7807235a4517cabd211c
ambika-gits <ambika05012006@gmail.com>
2026-07-23 20:23:17 +0530

    Initial commit: Django project

---
+++ mysite/blog/models.py
@@ -0,0 +1,51 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return(
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
+            
+class Post(models.Model):
+
+    # Status choices using TextChoices
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    # Fields
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body=models.TextField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
+    body=models.TextField()   
+    content = models.TextField()
+    
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    publish = models.DateTimeField(default=timezone.now)
+
+    # Status field
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
+    objects = models.Manager() 
+    published = PublishedManager() 
+    class Meta:
+        ordering = ['-publish']
+        indexes=[
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self):
+        return self.title
+# Create your models here.
